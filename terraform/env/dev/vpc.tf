8822b19f6287e9cd10aa42da01a92d02b967246a
Shaked <shakedkattan@outlook.com>
2025-07-26 19:38:22 +0300

    Update vpc.tf

--- terraform/env/dev/vpc.tf
+++ terraform/env/dev/vpc.tf
@@ -1,7 +1,7 @@
 module "dev_vpc" {
   source = "git::https://github.com/shakedkattan/automated_serverless_rds_cluster.git///terraform/modules/vpc?ref=main"
 
-  name                 = "prod"
+  name                 = "dev-vpc"
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidr   = "10.0.1.0/24"
   private_subnet_cidr  = "10.0.2.0/24"
