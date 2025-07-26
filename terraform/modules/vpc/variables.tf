9c070ea9be5a60162e8f00ad5d8a871e64535880
Shaked <shakedkattan@outlook.com>
2025-07-26 20:04:16 +0300

    Update variables.tf

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -13,19 +13,19 @@ variable "public_subnet_cidr" {
   type        = string
 }
 
-variable "private_subnet_cidr" {
-  description = "CIDR block for the private subnet"
-  type        = string
-}
-
 variable "public_subnet_az" {
   description = "Availability zone for the public subnet"
   type        = string
 }
 
-variable "private_subnet_az" {
+variable "private_subnet_cidrs" {
+  description = "CIDR block for the private subnet"
+  type        = list(string)
+}
+
+variable "private_subnet_azs" {
   description = "Availability zone for the private subnet"
-  type        = string
+  type        = list(string)
 }
 
 variable "enable_dns_support" {
