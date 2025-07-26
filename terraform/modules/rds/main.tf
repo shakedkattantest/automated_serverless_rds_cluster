e6892c45cca5d6f4cf40880ba1c4895e4a5d491a
Shaked <shakedkattan@outlook.com>
2025-07-26 20:31:52 +0300

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,1 +1,18 @@
+resource "aws_db_instance" "this" {
+  identifier             = "${var.name}-db"
+  allocated_storage      = var.allocated_storage
+  engine                 = var.engine
+  engine_version         = var.engine_version
+  instance_class         = var.instance_class
+  name                   = var.db_name
+  username               = var.username
+  password               = var.password
+  subnet_ids             = var.subnet_ids
+  vpc_security_group_ids = var.security_group_ids
+  skip_final_snapshot    = true
+  publicly_accessible    = false
 
+  tags = {
+    Name = "${var.name}-db"
+  }
+}
