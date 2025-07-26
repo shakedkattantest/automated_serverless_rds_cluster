3beb57d46b51ecff0adef67ec2f3aca357e24371
Shaked <shakedkattan@outlook.com>
2025-07-26 20:00:29 +0300

    Update vpc.tf

--- terraform/env/prod/vpc.tf
+++ terraform/env/prod/vpc.tf
@@ -3,8 +3,11 @@ module "prod_vpc" {
 
   name                 = "prod"
   vpc_cidr             = "10.0.0.0/16"
+
   public_subnet_cidr   = "10.0.1.0/24"
-  private_subnet_cidr  = "10.0.2.0/24"
   public_subnet_az     = "eu-central-1a"
-  private_subnet_az    = "eu-central-1b"
+
+  public_subnet_az     = "eu-central-1a"
+  private_subnet_cidrs  = ["10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_azs    = ["eu-central-1a", "eu-central-1b"]
 }
