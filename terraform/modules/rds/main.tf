a1ada7c681a67c0943b7ba9356e91418e3c95a1f
Shaked <shakedkattan@outlook.com>
2025-07-26 23:14:07 +0300

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -3,7 +3,6 @@ resource "aws_db_instance" "this" {
   allocated_storage      = var.allocated_storage
   engine                 = var.engine
   instance_class         = var.instance_class
-  name                   = var.db_name
   username               = var.username
   password               = var.password
   db_subnet_group_name   = var.db_subnet_group_name
