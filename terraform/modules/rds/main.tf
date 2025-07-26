6502033739a8b0ca0fccbfd6209ff38bf0ffe391
Shaked <shakedkattan@outlook.com>
2025-07-27 02:59:00 +0300

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "this" {
-  identifier             = "${var.name}-db"
+  identifier             = "${lower(var.name)}-db"
   allocated_storage      = var.allocated_storage
   engine                 = var.engine
   instance_class         = var.instance_class
