5c33af897b43f56917d405ef44d005d7f31aa20a
Shaked <shakedkattan@outlook.com>
2025-07-26 19:37:59 +0300

    Create vpc.tf

---
+++ terraform/env/prod/vpc.tf
@@ -0,0 +1,10 @@
+module "prod_vpc" {
+  source = "git::https://github.com/shakedkattan/automated_serverless_rds_cluster.git///terraform/modules/vpc?ref=main"
+
+  name                 = "prod"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidr   = "10.0.1.0/24"
+  private_subnet_cidr  = "10.0.2.0/24"
+  public_subnet_az     = "eu-central-1a"
+  private_subnet_az    = "eu-central-1b"
+}
