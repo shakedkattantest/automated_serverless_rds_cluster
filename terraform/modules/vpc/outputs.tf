5c3ed6695dd0bd73850b2def89ba16111e53cbb8
Shaked <shakedkattan@outlook.com>
2025-07-26 19:41:34 +0300

    Create outputs.tf

---
+++ terraform/modules/vpc/outputs.tf
@@ -0,0 +1,30 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr_block" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.public.id
+}
+
+output "private_subnet_id" {
+  description = "ID of the private subnet"
+  value       = aws_subnet.private.id
+}
+
+output "igw_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.igw.id
+}
+
+
+output "public_route_table_id" {
+  description = "Route table for public subnet"
+  value       = aws_route_table.public.id
+}
