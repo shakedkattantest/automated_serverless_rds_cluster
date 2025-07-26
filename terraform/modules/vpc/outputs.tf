826269ea4656eb5a7322c7132e7acc0143bb0ab0
Shaked <shakedkattan@outlook.com>
2025-07-26 20:06:02 +0300

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -14,8 +14,8 @@ output "public_subnet_id" {
 }
 
 output "private_subnet_id" {
-  description = "ID of the private subnet"
-  value       = aws_subnet.private.id
+  description = "IDs of the private subnets"
+  value       = [for subnet in aws_subnet.private : subnet.id]
 }
 
 output "igw_id" {
