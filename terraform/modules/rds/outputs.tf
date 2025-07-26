c06b04fb3eb1cd30478fe1524270f75209d2ab65
Shaked <shakedkattan@outlook.com>
2025-07-26 20:23:07 +0300

    Update outputs.tf

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,1 +1,10 @@
+output "db_instance_endpoint" {
+  value       = aws_db_instance.this.endpoint
+  description = "RDS endpoint"
+}
 
+
+output "db_instance_id" {
+  value       = aws_db_instance.this.id
+  description = "RDS instance ID"
+}
