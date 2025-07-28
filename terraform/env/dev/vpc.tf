b899ed37dc1f3fd2cb2f40e553ed5026becadc58
Shaked <shakedkattan@outlook.com>
2025-07-28 03:40:42 +0300

    Update vpc.tf

--- terraform/env/dev/vpc.tf
+++ terraform/env/dev/vpc.tf
@@ -1,10 +1,47 @@
-module "dev_vpc" {
+module "prod_vpc" {
   source = "git::https://github.com/shakedkattan/automated_serverless_rds_cluster.git///terraform/modules/vpc?ref=main"
 
-  name                 = "dev-vpc"
+  name                 = "dev"
   vpc_cidr             = "10.0.0.0/16"
+
   public_subnet_cidr   = "10.0.1.0/24"
-  private_subnet_cidr  = "10.0.2.0/24"
   public_subnet_az     = "eu-central-1a"
-  private_subnet_az    = "eu-central-1b"
+
+  private_subnet_cidrs  = ["10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_azs    = ["eu-central-1a", "eu-central-1b"]
+}
+
+
+resource "aws_db_subnet_group" "dev-rds" {
+  name       = "rds-subnet-group"
+  subnet_ids = module.dev_vpc.private_subnet_ids
+
+  tags = {
+    Name = "dev-rds-subnet-group"
+  }
+}
+
+
+resource "aws_security_group" "dev-rds" {
+  name        = "dev-rds-sg"
+  description = "Allow DB traffic"
+  vpc_id      = module.dev_vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = [module.dev_vpc.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dev-rds-sg"
+  }
 }
