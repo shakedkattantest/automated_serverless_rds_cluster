62c2cd9307ddb6369e6a07bb855453cec0ce3ca0
Shaked <shakedkattan@outlook.com>
2025-07-28 16:45:55 +0300

    Update main.tf

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,11 +1,11 @@
-data "aws_ssm_parameter" "db_username" {
-  name = "/project/db/username"
-}
+#data "aws_ssm_parameter" "db_username" {
+#  name = "/project/db/username"
+#}
 
-data "aws_ssm_parameter" "db_password" {
-  name            = "/project/db/password"
-  with_decryption = true
-}
+#data "aws_ssm_parameter" "db_password" {
+#  name            = "/project/db/password"
+#  with_decryption = true
+#}
 
 locals {
   mysql_list    = []
@@ -20,8 +20,10 @@ locals {
 
   shared_settings = {
     instance_class       = "db.t4g.micro"
-    db_username = data.aws_ssm_parameter.db_username.value
-    db_password = data.aws_ssm_parameter.db_password.value
+    #db_username = data.aws_ssm_parameter.db_username.value
+    #db_password = data.aws_ssm_parameter.db_password.value
+    username             = "masteruser"
+    password             = "masterpw"
     db_subnet_group_name = aws_db_subnet_group.dev_rds.name
     security_group_ids   = [aws_security_group.dev_rds.id]
     allocated_storage    = 20
