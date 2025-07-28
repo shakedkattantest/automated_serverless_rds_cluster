485bd10017cb1817f05c6757b4618e9d36903c9e
Shaked <shakedkattan@outlook.com>
2025-07-28 06:42:41 +0300

    Update main.tf

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,5 +1,5 @@
-resource "aws_db_instance" "this" {
-  identifier             = "${var.name}-db"
+resource "aws_db_instance" "main" {
+  identifier             = "${var.name}-main-db"
   allocated_storage      = var.allocated_storage
   engine                 = var.engine
   instance_class         = var.instance_class
@@ -8,8 +8,21 @@ resource "aws_db_instance" "this" {
   db_subnet_group_name   = var.db_subnet_group_name
   vpc_security_group_ids = var.security_group_ids
   publicly_accessible    = false
+  skip_final_snapshot     = true
 
   tags = {
-    Name = "${var.name}-db"
+    Name = "${var.name}-main-db"
+  }
+}
+
+resource "aws_db_instance" "read_replica" {
+  identifier           = "${var.name}-replica-db"
+  replicate_source_db  = aws_db_instance.main.id
+  instance_class       = var.instance_class  
+  publicly_accessible  = false
+  skip_final_snapshot  = true
+
+  tags = {
+    Name = "${var.name}-replica-db"
   }
 }
